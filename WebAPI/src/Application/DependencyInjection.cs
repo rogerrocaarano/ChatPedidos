b0ca7b5f28fc42bbc022ee0df50838ab59bad6ae
Roger Roca Arano <git@rogerroca.me>
2025-11-19 02:59:46 +0000

    feat: add DependencyInjection class for LiteBus module registration

---
+++ WebAPI/src/Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using LiteBus.Commands;
+using LiteBus.Extensions.Microsoft.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Register LiteBus modules
+        services.AddLiteBus(liteBus =>
+        {
+            var appAssembly = typeof(DependencyInjection).Assembly;
+
+            // Scan the assembly for all command/query/event handlers
+            liteBus.AddCommandModule(module => module.RegisterFromAssembly(appAssembly));
+            // liteBus.AddQueryModule(module => module.RegisterFromAssembly(appAssembly));
+            // liteBus.AddEventModule(module => module.RegisterFromAssembly(appAssembly));
+        });
+
+        return services;
+    }
+}
--- WebAPI/src/Apps/Api/Program.cs
+++ WebAPI/src/Apps/Api/Program.cs
@@ -1,8 +1,10 @@
+using Application;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
