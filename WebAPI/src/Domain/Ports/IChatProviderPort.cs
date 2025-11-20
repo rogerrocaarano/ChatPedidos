b4135078c34cc5c9626ae838a7c32341dccaa42f
Roger Roca Arano <git@rogerroca.me>
2025-11-20 07:52:43 +0000

    feat: extend IChatProviderPort interface
    - Add SendWelcomeMessageAsync method for welcome interactions
    - Add SendProductCatalogAsync method for product catalog display
    - Prepare interface for enhanced chat provider capabilities

--- WebAPI/src/Domain/Ports/IChatProviderPort.cs
+++ WebAPI/src/Domain/Ports/IChatProviderPort.cs
@@ -4,4 +4,6 @@ public interface IChatProviderPort<TId>
     where TId : IValueObject
 {
     Task SendMessageAsync(TId toId, string message);
+    Task SendWelcomeMessageAsync(TId toId);
+    Task SendProductCatalogAsync(TId toId);
 }
