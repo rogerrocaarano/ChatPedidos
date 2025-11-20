1352be3f391149659ff00a9246b5531aa6a7f86e
Roger Roca Arano <git@rogerroca.me>
2025-11-20 09:50:33 +0000

    feat: add SendMessageWithOptionsAsync method to ITelegramPort interface

--- WebAPI/src/Domain/Ports/ITelegramPort.cs
+++ WebAPI/src/Domain/Ports/ITelegramPort.cs
@@ -6,4 +6,5 @@ namespace Domain.Ports;
 public interface ITelegramPort
 {
     Task SendMessageAsync(TelegramId id, string message);
+    Task SendMessageWithOptionsAsync(TelegramId id, string message, ICollection<BotOption> options);
 }
